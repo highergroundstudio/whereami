fe30642cf99e5cdd912fe416eaa3a55b6a4b3734
Kyle King <kyle.king@highergroundstudio.com>
2017-03-27 15:30:51 -0600

    Intial commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+
+function geoFindMe() {
+  var output = document.getElementById("out");
+
+  if (!navigator.geolocation){
+    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+    return;
+  }
+
+  function success(position) {
+    var latLngPLS = new LatLng(position.coords.latitude, position.coords.longitude);
+    var utmPLS = latLngPLS.toUTMRef(new LatLng(position.coords.latitude, position.coords.longitude));
+    var utmString = utmPLS.toString();
+
+    acc = position.coords.accuracy;
+
+    // Get textback number from link
+    var tb = getQueryVariable("tb");
+    var tbTxt = '';
+    if(tb){
+      tbTxt = '<a class="button" href="sms://' + tb + '?body=' + utmString + '">Text your location back</a>';
+    }
+
+    output.innerHTML = '<h1>Your location is: ' + utmString + '</h1><p>With an accuracy of ' + acc + ' meters</p>' + tbTxt;
+
+  }
+
+  function error() {
+    output.innerHTML = "Unable to retrieve your location";
+  }
+
+  output.innerHTML = "<p>Locating…</p>";
+
+  navigator.geolocation.getCurrentPosition(success, error);
+}
+
+function getQueryVariable(variable)
+{
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if(pair[0] == variable){return pair[1];}
+  }
+  return(false);
+}
+
+geoFindMe();
